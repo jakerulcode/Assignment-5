db69ecd3f540087d5c3db477bf0d5c16cfb8811c
jakerulcode <jakerulislam024@gmail.com>
2026-03-08 23:11:51 +0600

    add all issue

--- main.js
+++ main.js
@@ -1,22 +1,70 @@
-const loadIssues =async ()=>{
-    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
-    const data =await res.json()
-    displayIssues(data.data)
+const loadIssues = async () => {
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await res.json();
+  displayIssues(data.data);
+  console.log(data);
+};
+// id": 1,
+// "title": "Fix navigation menu on mobile devices",
+// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+// "status": "open",
+// "labels": [
+// "bug",
+// "help wanted"
+// ],
+// "priority": "high",
+// "author": "john_doe",
+// "assignee": "jane_smith",
+// "createdAt": "2024-01-15T10:30:00Z",
+// "updatedAt": "2024-01-15T10:30:00Z"
 
-}
+const displayIssues = (datas) => {
+  console.log(datas);
+  const issuescontainer = document.getElementById("issues-container");
+  issuescontainer.innerHTML = "";
 
-const displayIssues = (issues)=>{
+  datas.forEach((data) => {
+    const statusIcon =
+      data.status === "open"
+        ? "./assets/green.png.png"
+        : "./assets/purple.png.png";
 
-    const issuescontainer = document.getElementById('issues-container');
-    issuescontainer.innerHTML = ''
+    const issuCard = document.createElement("div");
+    let buttonHtml = '';
+    data.labels.forEach(item=>{
+        buttonHtml+=`<button>${item}</button>`
+    })
 
-    issues.forEach((issue) => {
+    issuCard.innerHTML = `
+      <div class="bg-base-100 h-full shadow p-6 space-y-3">
+        <div class="flex justify-between items-center">
+          <img src="${statusIcon}" alt="" />
+          <span class="badge badge-error bg-red-100 text-red-500 border-none px-4 py-3">
+            ${data.priority}
+          </span>
+        </div>
 
-        const issuCard = document.createElement('div');
-        issuCard.innerHTML = `
-            
-        `
-        
-    });
+        <h2 class="text-[14px] font-semibold text-[#1F2937]">
+          ${data.title}
+        </h2>
 
-}
+        <p class="text-[#64748B] text-[12px] line-clamp-2">
+          ${data.description}
+        </p>
+
+        <div class="flex">${buttonHtml}</div>
+
+        <div class="border-t text-sm text-gray-500 text-left">
+          <p>${data.author}</p>
+          <p>${data.createdAt}</p>
+        </div>
+      </div>
+    `;
+
+    issuescontainer.appendChild(issuCard);
+  });
+};
+
+loadIssues();
